8f1c0a4426089496bef318c2e9c2bdd21699cc47
User <User@DESKTOP-5RTSMQ1>
2020-11-27 20:12:06 +0200

    pio

---
+++ meteo2/src/main.cpp
@@ -0,0 +1,11 @@
+#include <Arduino.h>
+
+void setup() {
+  Serial.begin(9600);
+  Serial.print("sun");
+  // put your setup code here, to run once:
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+}
