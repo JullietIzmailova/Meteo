4b1d791153bff448ffa5cb5831572df4e169eecd
Sergey Light <sergey.light@gmail.com>
2021-02-02 09:24:56 +0200

    Edit LED actuator

--- src/LEDActuator.cpp
+++ src/LEDActuator.cpp
@@ -15,3 +15,42 @@ bool LED_YELLOW_Setup()
 {
     pinMode(LED_YELLOW_PIN, OUTPUT);
 }
+
+void LED_Red_Set_Light(bool on)
+{
+    if(on)
+    {
+        digitalWrite(LED_RED_PIN, HIGH);
+    }
+    else
+    {
+        digitalWrite(LED_RED_PIN, LOW);
+    }
+    
+}
+
+void LED_Green_Set_Light(bool on)
+{
+    if(on)
+    {
+        digitalWrite(LED_GREEN_PIN, HIGH);
+    }
+    else
+    {
+        digitalWrite(LED_GREEN_PIN, LOW);
+    }
+    
+}
+
+void LED_Yellow_Set_Light(bool on)
+{
+    if(on)
+    {
+        digitalWrite(LED_YELLOW_PIN, HIGH);
+    }
+    else
+    {
+        digitalWrite(LED_YELLOW_PIN, LOW);
+    }
+    
+}
