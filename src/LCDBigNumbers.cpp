6ff9ccb5b5d4d4e04afac00d5026ecbeb7998804
User <User@DESKTOP-5RTSMQ1>
2021-02-19 16:48:39 +0200

    new block 5 and number 6 :P

--- src/LCDBigNumbers.cpp
+++ src/LCDBigNumbers.cpp
@@ -14,7 +14,7 @@ byte Block4x3[] = {
   B00000        
 };
 
-byte Block5x2[] = {
+byte Block5x2UpDown[] = {
   B11111,
   B11111,
   B00000,
@@ -61,6 +61,18 @@ byte Block4x3Down[] = {
   B11110
 };
 
+byte Block5x2Up[] = {
+  B11111,
+  B11111,
+  B00000,  
+  B00000,
+  B00000,
+  B00000,
+  B00000,
+  B00000        
+};
+
+
 bool LCDBigNumber_Setup()
 {
     
@@ -71,11 +83,12 @@ bool LCDBigNumber_Setup()
     }
     
     lcd->createChar(0, Block4x3);
-    lcd->createChar(1, Block5x2);
+    lcd->createChar(1, Block5x2UpDown);
     lcd->createChar(2, Block5x3Down);
     lcd->createChar(3, Block4x8);
     lcd->createChar(4, Block4x3Down);
-    
+    lcd->createChar(5, Block5x2Up);
+   
     return true;
 
 }
@@ -126,11 +139,34 @@ void Print_Number_3_h2(int column, int row)
     lcd->write(byte(3));
 }
 
+void Print_Number_6_h2(int column, int row)
+{
+    LiquidCrystal_I2C * lcd = LCD_Get_LCDPointer();
+//first line 
+    lcd->setCursor(column, row);
+    lcd->write(byte(3));
+
+    lcd->setCursor(column+1, row);
+    lcd->write(byte(5));
+
+    lcd->setCursor(column+2, row);
+    lcd->write(byte(0));
+//secont line     
+    lcd->setCursor(column, row + 1);
+    lcd->write(byte(3));
+
+    lcd->setCursor(column+1, row + 1);
+    lcd->write(byte(1));
+
+    lcd->setCursor(column+2, row + 1);
+    lcd->write(byte(3));
+}
 
 void LCDBigNumber_Print_Height2_Right(int row, String number)
 {
     Print_Number_3_h2(0, row);
     Print_Number_2_h2(3, row);
+    Print_Number_6_h2(6, row);
     
 
 }
