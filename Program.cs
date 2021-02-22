a2ea2b50d48015e3917ae177b9d2323193a035d5
kirindenis1977@gmail.com <kirindenis1977@gmail.com>
2021-02-22 14:25:52 +0200

    git update-index --assume-unchanged  C:\Users\Egor3\Source\Repos\JullietIzmailova\Meteo\.vs\slnx.sqlite

--- Program.cs
+++ Program.cs
@@ -6,6 +6,7 @@ namespace Meteo
     {
         static void Main(string[] args)
         {
+            //git update-index --assume-unchanged  C:\Users\Egor3\Source\Repos\JullietIzmailova\Meteo\.vs\slnx.sqlite
             Console.WriteLine("Meteo UART");
             Console.ReadLine();
         }
