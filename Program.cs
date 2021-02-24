24266e1a473f6a861f8fc581566e5ef3953c3b45
Sergey Light <sergey.light@gmail.com>
2021-02-24 22:34:03 +0200

    ADD uart

--- Program.cs
+++ Program.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.IO.Ports;
+using System.Threading;
 
 namespace Meteo
 {
@@ -10,7 +12,32 @@ namespace Meteo
 
             Console.WriteLine("Meteo UART");
 
+            SerialPort _serialPort = new SerialPort("COM10", 
+                                        9600, 
+                                        Parity.None,
+                                        8,
+                                        StopBits.One);
+            _serialPort.Handshake = Handshake.None;
+
+            _serialPort.DataReceived += new SerialDataReceivedEventHandler(sp_DataReceived);
+
+            _serialPort.Open();
+
+            while (true) { 
+            _serialPort.Write("AT+T?\n");
+                Thread.Sleep(5000);
+            }
+
             Console.ReadLine();
         }
+
+        static void sp_DataReceived(object sender, SerialDataReceivedEventArgs e)
+{
+            SerialPort sp = (SerialPort)sender;
+            String data = sp.ReadExisting();
+
+            Console.WriteLine(data);
+}
+
     }
 }
